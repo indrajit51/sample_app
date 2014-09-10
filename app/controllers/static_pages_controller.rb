77ff99967477d7ecf758ea32c7dc87d92539e8b8
Indrajit Gupta <indrajit51@gmail.com>
2014-09-10 19:05:50 +0530

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-  	
+ 
+  def help
+  end	
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,7 +27,18 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contacts")
     end
   end
+  describe "Help page" do
 
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
   describe "About page" do
 
     it "should have the content 'About Us'" do
